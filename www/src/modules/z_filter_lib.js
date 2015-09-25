56c307619b293682e37caf6ec9455c102306dc6e
zopelee <zopelee@gmail.com>
2015-09-25 11:18:47 +0800

    range filter.

--- www/src/modules/z_filter_lib.js
+++ www/src/modules/z_filter_lib.js
@@ -11,3 +11,13 @@ angular.module('zFilterLib').filter('dateFormat', function ($filter) {
     return str
   };
 });
+
+angular.module('zFilterLib').filter('range', function () {
+  return function (input, total) {
+    total = parseInt(total);
+    for (var i = 0; i < total; i++) {
+      input.push(i);
+    }
+    return input;
+  };
+});
